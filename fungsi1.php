72f2791d0f11b55566bc337c6fdda91c69d63c72
romi7kb <romiramdhani84@gmail.com>
2019-08-29 16:49:32 +0700

    fungsi

---
+++ fungsi1.php
@@ -0,0 +1,11 @@
+<?php
+function cetak_ganjil (){
+    for ($i=0; $i <100 ; $i++) { 
+        if ($i%2==1) {
+            echo $i."<br>";
+        }
+    }
+}
+// pemangilan fungsi 
+cetak_ganjil();
+?>
---
+++ fungsi3.php
@@ -0,0 +1,7 @@
+<?php
+function luas_lingkaran($jari){
+    return 3.14*$jari*$jari;
+}
+// pemanggilan fungsi
+$r=10;
+echo "Luas lingkaran dengan jari jari $r = ".luas_lingkaran($r);
